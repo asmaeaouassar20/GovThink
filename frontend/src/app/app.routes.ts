173e6ca3a451858566b76d236aff5c69486ef975
asmae <asmaeaouassar333@example.com>
2025-07-11 22:42:43 +0100

    commencer le html de accueil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './authentification/register/register.component';
 import { LoginComponent } from './authentification/login/login.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 export const routes: Routes = [
 
     { path:'home' , component : HomeComponent}, //page d'acceuil
     { path:'register' , component : RegisterComponent},
-    { path:'login', component:LoginComponent}
+    { path:'login', component:LoginComponent},
+
+
+
+    // Composants
+    { path:'accueil', component:AccueilComponent},
+
+
 ];

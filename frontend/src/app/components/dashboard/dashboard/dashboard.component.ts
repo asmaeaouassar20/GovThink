37271e1e7fb885f853014dc1a698c50c49f64674
asmae <asmaeaouassar333@example.com>
2025-07-26 00:53:30 +0100

    ajouter une navigation de Tables et Charts dans le composant parent dashboard

--- frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../../sidebars/sidebar/sidebar.component";
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SidebarComponent, RouterLink],
+  imports: [SidebarComponent, RouterLink,RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })

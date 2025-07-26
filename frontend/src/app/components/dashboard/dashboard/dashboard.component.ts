5c19fdbca968ff90e33dd2699ce0a7ff48ff4e7c
asmae <asmaeaouassar333@example.com>
2025-07-26 18:16:49 +0100

    récupération des fichiers depuis le backend aumoment de l'initialisation du composant

--- frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SidebarComponent } from "../../../sidebars/sidebar/sidebar.component";
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { DashboardService } from '../../../service/dashboard.service';
+import { error } from 'console';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SidebarComponent, RouterLink,RouterOutlet],
+  imports: [SidebarComponent, RouterLink,RouterOutlet,NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  files : any[] = [];
 
+
+  dashboardService = inject(DashboardService);
+
+
+  ngOnInit(): void {
+      this.lireFichers();
+  }
+
+
+  lireFichers(){
+    this.dashboardService.getAllFiles().subscribe({
+
+      next : (res)=>{
+        this.files=res;
+      },
+
+      error : (error)=>{
+        console.error("Erreur dans la récupération des fichiers");
+      }
+    })
+  }
 }

23388aa42c6f196ade3ae071bc6312b329dcf8f8
asmae <asmaeaouassar333@example.com>
2025-07-28 20:39:09 +0100

    ajouter les repositories pour les posts

---
+++ backend/src/main/java/com/algostyle/backend/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.algostyle.backend.repository;
+
+import com.algostyle.backend.model.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
---
+++ backend/src/main/java/com/algostyle/backend/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.algostyle.backend.repository;
+
+import com.algostyle.backend.model.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}

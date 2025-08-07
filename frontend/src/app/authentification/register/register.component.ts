5069d681cf60169daa6b58e5c8e471708df004d5
asmae <asmaeaouassar333@example.com>
2025-08-07 15:58:04 +0100

    ajouter un lien vers l'acceuil

--- frontend/src/app/authentification/register/register.component.ts
+++ frontend/src/app/authentification/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule,NgIf,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })

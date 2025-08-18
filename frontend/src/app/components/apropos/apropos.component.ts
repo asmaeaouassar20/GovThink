cc102dfd26ca997208d1b4d3189dd8cce2b0355f
asmae <asmaeaouassar333@example.com>
2025-08-18 03:13:46 +0100

    ajouter les objectifs de la plateforme pour chaque profile

--- frontend/src/app/components/apropos/apropos.component.ts
+++ frontend/src/app/components/apropos/apropos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SidebarComponent } from "../../sidebars/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-apropos',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, RouterLink],
   templateUrl: './apropos.component.html',
   styleUrl: './apropos.component.css'
 })
